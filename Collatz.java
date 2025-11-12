78481537521957869850fbc9b50a4b374e6d4cba
shiraz-naomi <shiraz.ross@post.runi.ac.il>
2025-11-12 11:03:16 +0200

    nothing

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,25 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		String word = args[0];
+		int n = Integer.parseInt(args[1]);
+		word = word.toUpperCase();
+		String useAn = "AEFHILMNORSX";
+
+		for (int i = 0; i < word.length(); i++) {
+			System.out.print("Give me ");
+			char c = word.charAt(i);
+			if (useAn.indexOf(c) != -1) {
+				System.out.println("an " + c + "!");
+			} else {
+				System.out.println("a " + c + "!");
+			}
+
+		}
+
+		System.out.println("What does that spell?");
+		for (int i = 0; i < word.length(); i++) {
+			System.out.println(word + "!!!");
+		}
 	}
 }
