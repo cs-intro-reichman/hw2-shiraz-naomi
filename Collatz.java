9d2c68e4bd8aadcdda51177bb8ba0d0b16a4a93a
shiraz-naomi <shiraz.ross@post.runi.ac.il>
2025-11-12 23:16:34 +0200

    Update Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,27 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
+		int num = Integer.parseInt(args[0]);
+		String type = args[1];
+		int current = 0;
+		if (type.equals("v")) {
+			for (int j = 1; j <= num; j++) {
+				current = j;
+				int count = 1;
+				System.out.print(current);
+				while (current != 1) {
+					if (current % 2 == 0) {
+						current = current / 2;
+					} else {
+						current = current * 3 + 1;
+					}
+					System.out.print(" " + current);
+					count++;
+				}
 
+				System.out.println("(" + count + ")");
+			}
+		}
+		System.out.println("Every one of the first " + num + " hailstone sequences reached 1.");
 	}
 }
