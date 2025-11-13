eedfaabfce0c35a77d2aa0adb6eb14dba62a121b
shiraz-naomi <shiraz.ross@post.runi.ac.il>
2025-11-13 09:09:37 +0200

    Update CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -3,12 +3,11 @@ public class CalcPi {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 		System.out.println("pi according to Java: " + Math.PI);
-		double sum = 0;
+		double sum = 0.0;
 		for (int i = 0; i < n; i++) {
 			sum += Math.pow(-1, i) / (2 * i + 1);
 
 		}
-		System.out.println("approximated:     " + (sum * 4));
-
+		System.out.println("pi, approximated:     " + (sum * 4));
 	}
 }
